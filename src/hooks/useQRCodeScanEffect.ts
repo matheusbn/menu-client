1583007471bceef6d67191ca2f41bcbda1cd3342
Matheus Berkenbrock Nedel <matheusbnedel@gmail.com>
2020-06-27 23:26:36 -0300

    use lodash isFunction

--- src/hooks/useQRCodeScanEffect.ts
+++ src/hooks/useQRCodeScanEffect.ts
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
-import { isFunction } from "helpers/utils"
+import { useEffect } from 'react'
+import isFunction from 'lodash/isFunction'
 import jsQR from 'jsqr-es6'
 // import QrScanner from '../../node_modules/qr-scanner/qr-scanner-worker.min.js'
 
 export default function useQRScannerEffect(videoRef, handler, deps = []) {
   useEffect(() => {
-    if(!videoRef.current) return
+    if (!videoRef.current) return
 
     const video = videoRef.current
     const canvas = document.createElement('canvas')
     const ctx = canvas.getContext('2d')
 
-    let intervalID = null
+    let intervalID
     const canPlayHandler = () => {
       intervalID = setInterval(() => {
         if (video.readyState === video.HAVE_ENOUGH_DATA) {
@@ -27,7 +27,7 @@ export default function useQRScannerEffect(videoRef, handler, deps = []) {
           const code = jsQR(imageData.data, imageData.width, imageData.height)
 
           if (isFunction(handler)) handler(code)
-          if(code) console.log(code)
+          if (code) console.log(code)
         }
       }, 200)
     }
--- src/hooks/useSetState.ts
+++ src/hooks/useSetState.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { isFunction } from 'helpers/utils'
+import isFunction from 'lodash/isFunction'
 
 export default initialState => {
   const [state, setState] = useState(initialState)
