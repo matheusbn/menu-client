35bfad8db6020a8d9e3195bcd502f4108a3d1cbf
Matheus Berkenbrock Nedel <matheusbnedel@gmail.com>
2020-06-29 16:40:31 -0300

    fix staging order not clearing after confirming order

--- src/actions/restaurant.ts
+++ src/actions/restaurant.ts
@@ -1,12 +1,14 @@
-// export const addOrder = (order: Order) => async (dispatch, getState) => {
-//   if (!order) {
-//     throw Error(`expected an order as parameter. Received: ${order}`)
-//     return
-//   }
+export const addOrder = (order: Order) => async (dispatch, getState) => {
+  if (!order) {
+    throw Error(`expected an order as parameter. Received: ${order}`)
+  }
+  dispatch({
+    type: 'ADD_ORDER',
+  })
 
-//   const { restaurant } = getState()
-//   restaurant.currentSession.addOrder(order)
-// }
+  const { restaurant } = getState()
+  restaurant.currentSession.addOrder(order)
+}
 
 export const checkout = (totalPrice: number) => async (dispatch, getState) => {
   const { restaurant } = getState()
--- src/pages/stagingOrder/index.tsx
+++ src/pages/stagingOrder/index.tsx
@@ -4,7 +4,7 @@ import { Button, Typography } from '@material-ui/core'
 import ItemOrderList from 'components/ItemOrderList'
 import AppBar from 'components/AppBar'
 import BottomBar from 'components/BottomBar'
-// import { addOrder } from 'actions'
+import { addOrder } from 'actions'
 import { useSelector, useDispatch } from 'react-redux'
 
 const useStyles = makeStyles(theme => ({
@@ -28,14 +28,14 @@ const useStyles = makeStyles(theme => ({
 function StagingOrder(props) {
   const classes = useStyles()
   const stagingOrder: Order = useSelector(state => state.stagingOrder)
-  const session = useSelector(state => state.restaurant.currentSession)
   const dispatch = useDispatch()
 
   const handleConfirm = () => {
-    stagingOrder.orderedAt = new Date()
-
-    session.addOrder(stagingOrder)
     history.back()
+
+    stagingOrder.orderedAt = new Date()
+    // prevent list of items disappearing before the page changes
+    setTimeout(() => dispatch(addOrder(stagingOrder)), 50)
   }
 
   return (
